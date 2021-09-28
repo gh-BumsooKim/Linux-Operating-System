bc779ff554730ddc2f61d4f9b3877f4ec5bb3d48
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-09-29 05:16:50 +0900

    Update pexec.c

--- process/pexec.c
+++ process/pexec.c
@@ -1,15 +1,29 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-	char *const ps_argv[] = {"ps", "-ax", 0};
+        char input[10];
+        char arg1[10];          // first arg
+        char arg2[] = "-l";     // seconde arg
+        char *arg;              // strtok pointer
 
-	printf("exec system call function\n");
+        printf("exec system call function\n");
 
-	//execl("bin/ps", "ps", "-ax", 0);
-	execv("bin/ps", ps_argv);
+        gets(input);
 
-	printf("Done\n");
-	exit(0);
+        // first argument : command
+        arg = strtok(input, " ");
+        if (arg != NULL)
+                strcpy(arg1, arg);
+
+        // seconde argument : options
+        arg = strtok(NULL, " ");
+        if (arg != NULL)
+                strcpy(arg2, arg);
+
+        execlp(arg1, arg1, arg2, NULL);
+
+        exit(0);
 }
